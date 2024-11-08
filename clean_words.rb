9d51e0a6a0c3c10e94b8c2eec37b0a39807903af
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-08 00:26:57 -0600

    Clean word list

---
+++ clean_words.rb
@@ -0,0 +1,4 @@
+filedata = File.readlines("words.txt").map(&:chomp).select {|str| str.length < 13 && str.length > 4}
+File.open("cleaned_words.txt", "w") do |file|
+  filedata.each {|line| file.puts(line)}
+end
