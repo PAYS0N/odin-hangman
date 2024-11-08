b8e78a9830b0192dab66aa022b862cf9d4bb44af
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-08 12:33:28 -0600

    Add bundle files

--- clean_words.rb
+++ clean_words.rb
@@ -1,4 +1,6 @@
-filedata = File.readlines("words.txt").map(&:chomp).select {|str| str.length < 13 && str.length > 4}
+# frozen_string_literal: true
+
+filedata = File.readlines("words.txt").map(&:chomp).select { |str| str.length < 13 && str.length > 4 }
 File.open("cleaned_words.txt", "w") do |file|
-  filedata.each {|line| file.puts(line)}
+  filedata.each { |line| file.puts(line) }
 end
